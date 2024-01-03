bbea04d2ee892f69180bb900197df952e9e296e0
Sergio Colque <raidanearth@gmail.com>
2024-01-03 15:07:07 -0300

    DER Checkear

--- src/mainb.cpp
+++ src/mainb.cpp
@@ -249,33 +249,133 @@ int main()
         Personaje(1, "Superhéroe", 1, "Serie A"),
         Personaje(2, "Villano", 2, "Serie B")};
 
-    // Serializamos las instancias a un archivo
-    std::ofstream ofs("datos.bin");
-    boost::archive::text_oarchive oa(ofs);
-    oa << actores;
-    oa << personajes;
-    ofs.close();
+    std::vector<Proyecto> proyectos = {
+        Proyecto(1, "Proyecto 1", "row_1"),
+        Proyecto(2, "Proyecto 2", "row_2")};
 
-    // Creamos nuevos vectores para recibir los datos desde el archivo
-    std::vector<Actor> actoresCargados;
-    std::vector<Personaje> personajesCargados;
+    std::vector<Guion> guiones = {
+        Guion(1, 1, "texto_plano_1"),
+        Guion(2, 2, "texto_plano_2")};
+}
 
-    // Deserializamos las instancias desde el archivo
-    std::ifstream ifs("datos.bin");
-    boost::archive::text_iarchive ia(ifs);
-    ia >> actoresCargados;
-    ia >> personajesCargados;
+std::vector<Escena> escenas = {
+    Escena(1, "Escena 1", 1, 1, "tipo_1", "tiempo_1"),
+    Escena(2, "Escena 2", 2, 2, "tipo_2", "tiempo_2")};
+}
 
-    // Imprimirmos los datos de los nuevos vectores para verificar
-    for (const auto &actor : actoresCargados)
-    {
-        std::cout << "Actor: " << actor.nombre << " " << actor.apellido << std::endl;
-    }
+std::vector<Toma> tomas = {
+    Toma(1, "Toma 1", 1, "encuadre_1"),
+    Toma(2, "Toma 2", 2, "encuadre_2")};
+}
 
-    for (const auto &personaje : personajesCargados)
-    {
-        std::cout << "Personaje: " << personaje.nombre << " en " << personaje.serie << std::endl;
-    }
+std::vector<Actuacion> actuaciones = {
+    Actuacion(1, 1, 1),
+    Actuacion(2, 2, 2)};
 
-    return 0;
+std::vector<Locacion> locaciones = {
+    Locacion(1, "Locacion 1", "direccion_1", "telefono_1", "hospital_cer_1", "estacionamiento_1"),
+    Locacion(2, "Locacion 2", "direccion_2", "telefono_2", "hospital_cer_2", "estacionamiento_2")};
+
+std::vector<Objeto> objetos = {
+    Objeto(1, "Objeto 1", 1, "descripcion_1", "tipo_1"),
+    Objeto(2, "Objeto 2", 2, "descripcion_2", "tipo_2")};
+}
+
+std::vector<Uso_Ficcional> usos_ficcionales = {
+    Uso_Ficcional(1, 1, 1),
+    Uso_Ficcional(2, 2, 2)};
+
+std::vector<Uso_Tecnico> usos_tecnicos = {
+    Uso_Tecnico(1, 1, 1),
+    Uso_Tecnico(2, 2, 2)};
+}
+
+// Serializamos las instancias a un archivo
+std::ofstream ofs("datos.bin");
+boost::archive::text_oarchive oa(ofs);
+oa << actores;
+oa << personajes;
+oa << guions;
+oa << escenas;
+oa << tomas;
+oa << actuaciones;
+oa << locaciones;
+oa << objetos;
+oa << usos_ficcionales;
+oa << usos_tecnicos;
+ofs.close();
+
+// Creamos nuevos vectores para recibir los datos desde el archivo
+std::vector<Actor> actoresCargados;
+std::vector<Personaje> personajesCargados;
+std::vector<Guion> guionesCargados;
+std::vector<Escena> escenasCargadas;
+std::vector<Toma> tomasCargadas;
+std::vector<Actuacion> actuacionesCargadas;
+std::vector<Locacion> locacionesCargadas;
+std::vector<Objeto> objetosCargados;
+std::vector<Uso_Ficcional> usos_ficcionalesCargados;
+std::vector<Uso_Tecnico> usos_tecnicosCargados;
+
+// Deserializamos las instancias desde el archivo
+std::ifstream ifs("datos.bin");
+boost::archive::text_iarchive ia(ifs);
+ia >> actoresCargados;
+ia >> personajesCargados;
+ia >> guionesCargados;
+ia >> escenasCargadas;
+ia >> tomasCargadas;
+ia >> actuacionesCargadas;
+ia >> locacionesCargadas;
+ia >> objetosCargados;
+ia >> usos_ficcionalesCargados;
+ia >> usos_tecnicosCargados;
+
+// Imprimirmos los datos de los nuevos vectores para verificar
+for (const auto &actor : actoresCargados)
+{
+    std::cout << "Actor: " << actor.nombre << " " << actor.apellido << std::endl;
+}
+
+for (const auto &personaje : personajesCargados)
+{
+    std::cout << "Personaje: " << personaje.nombre << " en " << personaje.serie << std::endl;
+}
+
+for (const auto &guion : guionesCargados)
+{
+    std::cout << "Guion: " << guion.texto_plano << std::endl;
+}
+
+for (const auto &escena : escenasCargadas)
+{
+    std::cout << "Escena: " << escena.numero << " en " << escena.tiempo << std::endl;
+}
+
+for (const auto &toma : tomasCargadas)
+{
+    std::cout << "Toma: " << toma.numero << " en " << toma.encuadre << std::endl;
+}
+
+for (const auto &actuacion : actuacionesCargadas)
+{
+    std::cout << "Actuacion: " << actuacion.id << " en " << actuacion.escenaId << std::endl;
+}
+
+for (const auto &locacion : locacionesCargadas)
+{
+    std::cout << "Locacion: " << locacion.nombre << " en " << locacion.direccion << std::endl;
+}
+
+for (const auto &objeto : objetosCargados)
+{
+    std::cout << "Objeto: " << objeto.nombre << " en " << objeto.descripcion << std::endl;
+}
+
+for (const auto &uso_ficcional : usos_ficcionalesCargados)
+{
+    std::cout << "Uso Ficcional: " << uso_ficcional.id << " en " << uso_ficcional.objetoId << std::endl;
+}
+
+return 0;
 }
